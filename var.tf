3296264e3733cf6138df73cca3a65fd3b7866e1e
Isaac Adanyeguh <nyeguh@yahoo.com>
2023-01-30 05:36:29 -0600

    modified code

---
+++ var.tf
@@ -0,0 +1,10 @@
+variable "key_pair_name" {
+    type = string
+    description = "keypair to utilize"
+    default = "ec2key-virginia-east1"
+}
+
+# variable "vpc_id" {
+#     type = string
+#     description = "vpc id to create the security group"
+# }
